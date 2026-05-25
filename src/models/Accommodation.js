4bd505cced8966b8df615ba12f8c055d8b793038
Samuel <samuel_somy93@hotmail.com>
2026-05-25 09:45:03 +0200

    feat: add Accommodation model with schema definition

---
+++ src/models/Accommodation.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const accommodationSchema = new mongoose.Schema(
+    {
+        address: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        country: {
+            type: String,
+            required: true,
+        },
+        postalCode: {
+            type: String,
+            required: true,
+        },
+        rent: {
+            type: Number,
+            required: true,
+        },
+        rooms: {
+            type: Number,
+            required: true,
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("Accommodation", accommodationSchema);
