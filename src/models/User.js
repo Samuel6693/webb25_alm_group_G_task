54fab49dfc073d495a914c3648187fc5da3b6e26
Lily Laptop <lily.weathers87@gmail.com>
2026-05-25 09:37:45 +0200

    refactor: migrate User model to ES module syntax and update schema fields

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,23 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: true,
-    },
     email: {
       type: String,
       required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Invalid email format"],
     },
-    // TODO: Add profileImage field
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    }
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+export default mongoose.model("User", userSchema);
